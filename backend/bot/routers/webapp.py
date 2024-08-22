f9e6a86a9ccceeba94b7c94033b164cdbf0b842f
Захар Димидов <zahardimidov@MacBook-Air-Zahar.local>
2024-08-22 18:07:50 +0500

    commit

--- backend/bot/routers/webapp.py
+++ backend/bot/routers/webapp.py
@@ -12,7 +12,7 @@ async def video_receive(message: Message):
     data = json.loads(message.web_app_data.data)
     video = await youtube_get_video(data['id'])
 
-    duration = ''.join([i if i.isdigit() else ':' for i in duration[2:-1]])
+    duration = ''.join([i if i.isdigit() else ':' for i in video['duration'][2:-1]])
 
     msg = f'\U0001F37F <b><a href="https://www.youtube.com/watch?v={video["id"]}">{video["title"]}</a></b>\n\n\U0001F5E3 Автор: #{video["channel_title"]}\n\U0001F4C5 Дата: {video["publishedAt"]}\n \u23F1 Продолжительность: {duration}'
     await message.answer_photo(photo=data['photo'], caption=msg)
