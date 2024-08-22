278c492e47e6dbe313c5afc2592c7b8b251d0494
Захар Димидов <zahardimidov@MacBook-Air-Zahar.local>
2024-08-22 18:06:13 +0500

    commit

--- downloader/run.py
+++ downloader/run.py
@@ -3,6 +3,17 @@ import threading
 import yt_dlp
 import os
 from dotenv import load_dotenv
+import pathlib
+
+
+video_folder = pathlib.Path(__file__).parent.parent.resolve().joinpath('video')
+audio_folder = pathlib.Path(__file__).parent.parent.resolve().joinpath('audio')
+
+if not os.path.exists(video_folder):
+    os.mkdir(video_folder)
+    
+if not os.path.exists(audio_folder):
+    os.mkdir(audio_folder)
 
 load_dotenv()
 
@@ -11,29 +22,37 @@ HOST = os.environ.get('HOST', 'localhost')
 # Connect to Redis server
 r = redis.Redis(host=HOST, port=6379, db=0)
 
-def download(video_id):
+def download_video(video_id):
     ydl_opts = {
         'format': "bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best",
-        "outtmpl": 'videos/%(id)s.%(ext)s',
+        "outtmpl": f'{video_folder}/%(id)s.%(ext)s',
     }
 
-    filename = f'videos/{video_id}.mp4'
+    filename = f'{video_folder}/{video_id}.mp4'
 
     if not os.path.exists(filename):
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             ydl.download([f'https://www.youtube.com/watch?v={video_id}'])
             print('Complete loading')
 
+def download_audio(video_id):
+    ...
+
 print('DOWNLOADER STARTED')
 
 while True:
-    # Get a task from the queue
-    task: bytes = r.lpop('tasks')
-    
-    # Process the task
+    task: bytes = r.lpop('download_video')
     if task is not None:
         video_id = task.decode()
-        target = threading.Thread(target=download, name=f'download_video_{video_id}', args=[video_id])
+        target = threading.Thread(target=download_video, name=f'download_video_{video_id}', args=[video_id])
         target.start()
 
-        print('Task started: %s' % task)
+        print('Video downloading started: %s' % task)
+
+    task: bytes = r.lpop('download_audio')
+    if task is not None:
+        video_id = task.decode()
+        target = threading.Thread(target=download_video, name=f'download_audio_{video_id}', args=[video_id])
+        target.start()
+
+        print('Audio downloading started: %s' % task)
