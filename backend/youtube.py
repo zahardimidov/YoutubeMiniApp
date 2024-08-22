274406b3637c26ce344255d3385a4b5d85e5db41
Захар Димидов <zahardimidov@MacBook-Air-Zahar.local>
2024-08-22 21:51:48 +0500

    commit

--- backend/youtube.py
+++ backend/youtube.py
@@ -72,7 +72,7 @@ async def youtube_get_video(video_id='9GeW5T-c1Yw'):
 
     if video:
         for i in ['default', 'medium', 'high', 'standart', 'maxres']:
-            if i in data['items'][0]['snippet']['thumbnails']:
+            if i in list(data['items'][0]['snippet']['thumbnails'].keys()):
                 video['photo'] = data['items'][0]['snippet']['thumbnails'][i]['url']
     return video
 
