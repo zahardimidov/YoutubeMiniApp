b5f0c4f3254df07dd00a76774f9dcce8713084b0
Захар Димидов <zahardimidov@MacBook-Air-Zahar.local>
2024-08-28 02:23:49 +0500

    commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,7 +11,7 @@ function App() {
         <Route path="/" exact={true} element={<Home />} />
         <Route path="/channel" element={<Channel />} />
         <Route path="/download" element={<Downloading />} />
-        <Route path="/check_audio/:video_id/:format_id" element={<Downloading type='audio'/>} />
+        <Route path="/check_audio/:video_id/:format_id" element={<Downloading/>} />
       </Routes>
     </BrowserRouter>
   );
