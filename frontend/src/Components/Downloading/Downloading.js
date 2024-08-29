649814a8dd0941d9e4d78ba9e222f1ab006dd38e
Захар Димидов <zahardimidov@MacBook-Air-Zahar.local>
2024-08-30 00:50:15 +0500

    commit

--- frontend/src/Components/Downloading/Downloading.js
+++ frontend/src/Components/Downloading/Downloading.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { useSearchParams } from "react-router-dom";
-import Loading from '../Loading/Loading';
 import './Downloading.css';
 import loadGIF from '../../Assets/gif/load.gif'
 import waitGIF from '../../Assets/gif/wait.gif'
@@ -9,7 +8,6 @@ import problemGIF from '../../Assets/gif/problem.gif'
 
 function Downloading() {
     const [text, setText] = React.useState('Файл подготавливается');
-    const [loading, setLoading] = React.useState(true);
     const [searchParams, setSearchParams] = useSearchParams();
     const [gif, setGif] = React.useState(loadGIF);
 
@@ -30,7 +28,6 @@ function Downloading() {
                 .then(data => {
                     if (data.status == 'ready') {
                         clearInterval(interval);
-                        setLoading(false);
                         setGif(waitGIF);
 
                         setText('Скачивание скоро начнется');
@@ -38,7 +35,6 @@ function Downloading() {
                     }
                     if (data.status == 'subscribe') {
                         clearInterval(interval);
-                        setLoading(false);
                         setGif(problemGIF);
 
                         setText('Подписка не активна, вернитесь в бот и оплатите подписку !!!');
@@ -52,7 +48,6 @@ function Downloading() {
     return (
         <div className='downloading'>
             <h1 style={{ color: 'white', textAlign: 'center', paddingBlock: '20vh 5vh' }}>{text}</h1>
-            {loading && <Loading></Loading>}
             <a id='download' href={downloadURL} download={true}></a>
 
             <div className='center gif'>
