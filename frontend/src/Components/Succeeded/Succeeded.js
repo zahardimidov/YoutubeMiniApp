56e962d13ddac7ff86ec2b7d8c8befd797c9ae64
Захар Димидов <zahardimidov@MacBook-Air-Zahar.local>
2024-08-30 01:23:05 +0500

    commit

--- frontend/src/Components/Succeeded/Succeeded.js
+++ frontend/src/Components/Succeeded/Succeeded.js
@@ -3,11 +3,11 @@ import coolGIF from '../../Assets/gif/cool.gif'
 
 function Succeeded() {
     return (
-        <div className='succeeded' style={{ color: 'white', textAlign: 'center', paddingBlock: '35vh' }}>
+        <div className='succeeded' style={{ color: 'white', textAlign: 'center', paddingBlock: '25vh' }}>
             <h1>Платеж выполнен успешно</h1>
             <h3 style={{ marginTop: '15px' }}>Теперь вы можете наслаждаться возможностью подписки в боте</h3>
 
-            <div className='center'>
+            <div className='center gif'>
                 <img src={coolGIF} alt="succeeded"></img>
             </div>
         </div>
