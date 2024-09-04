0a0e11b2b3b1cbe10d41e9022a5fba8489c1ef63
Захар Димидов <zahardimidov@MacBook-Air-Zahar.local>
2024-09-04 22:53:36 +0500

    commit

--- downloader/run.py
+++ downloader/run.py
@@ -56,6 +56,9 @@ def download_video(data):
         ]
 
         subprocess.run(command)
+
+        time.sleep(3)
+        
         subprocess.run(['mv', f'{video_folder}/{video_id}_{video_format}_combine.mp4', f'{video_folder}/{video_id}_{video_format}.mp4'])
 
         time.sleep(3)
