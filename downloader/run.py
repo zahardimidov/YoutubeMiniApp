7aa7a57b606ce3447f4fbb3e229a8de68b383be9
Захар Димидов <zahardimidov@MacBook-Air-Zahar.local>
2024-09-12 22:10:09 +0500

    commit

--- downloader/run.py
+++ downloader/run.py
@@ -15,16 +15,6 @@ import atexit
 api_id = '20985389'
 api_hash = 'e29ea4c9df52d3f99fc0678c48a82da2'
 
-
-if __name__ == '__main__':
-    async def main():
-        userbot = Client("USERBOT", api_id, api_hash)
-
-        async with userbot:
-            print(await userbot.get_me())
-    asyncio.run(main())
-    exit()
-
 loop = asyncio.new_event_loop()
 asyncio.set_event_loop(loop)
 
