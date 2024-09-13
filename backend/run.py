593063978cca48225181fd278799b8dd8579c781
Захар Димидов <zahardimidov@MacBook-Air-Zahar.local>
2024-09-13 20:06:49 +0500

    commit

--- backend/run.py
+++ backend/run.py
@@ -28,7 +28,6 @@ async def on_startup(app: FastAPI):
     init_admin(app=app, engine=engine)
     await run_database()
     await run_bot()
-    await userbot.start()
     
     asyncio.create_task(runner.run_main())
 
