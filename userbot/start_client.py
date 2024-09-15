b64d109f5dd13165433ef765501e7a84076e687c
Захар Димидов <zahardimidov@MacBook-Air-Zahar.local>
2024-09-15 15:50:21 +0500

    commit

--- userbot/start_client.py
+++ userbot/start_client.py
@@ -7,11 +7,14 @@ BASE_DIR = pathlib.Path(__file__).parent.parent.resolve()
 API_ID = '20985389'
 API_HASH = 'e29ea4c9df52d3f99fc0678c48a82da2'
 
+# +79600471358
+
 client = TelegramClient('USERBOT', API_ID, API_HASH)
-client.start()
 
 
 async def main():
+    client.start()
+    
     me = await client.get_me()
     print(me)
 
