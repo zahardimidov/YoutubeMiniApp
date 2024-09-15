48421d0570cf5c620ef3334c37a6b62eb075ecc8
Захар Димидов <zahardimidov@MacBook-Air-Zahar.local>
2024-09-15 15:47:53 +0500

    commit

---
+++ userbot/start_client.py
@@ -0,0 +1,18 @@
+import asyncio
+import pathlib
+
+from telethon import TelegramClient
+
+BASE_DIR = pathlib.Path(__file__).parent.parent.resolve()
+API_ID = '20985389'
+API_HASH = 'e29ea4c9df52d3f99fc0678c48a82da2'
+
+client = TelegramClient('USERBOT', API_ID, API_HASH)
+client.start()
+
+
+async def main():
+    me = await client.get_me()
+    print(me)
+
+asyncio.run(main())
